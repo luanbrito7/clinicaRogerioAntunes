c663d1bc74602141f18cd7e90502f309f60a677b
Luan Brito <lab7@cin.ufpe.br>
2018-05-02 18:44:10 +0000

    add get register to views

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var passport = require("passport");
-var Paciente = require("../models/paciente");
-var Funcionario = require("../models/funcionario");
+var User = require("../models/user");
+// var Funcionario = require("../models/funcionario");
 var Feedback = require("../models/feedback");
 
 router.get("/", function(req, res){
@@ -10,22 +10,25 @@ router.get("/", function(req, res){
 });
 
 //registro de paciente
-router.get("/register/paciente", function(req, res) {
-    res.render("registerPaciente");
+router.get("/register", function(req, res) {
+    res.render("register");
 });
-router.post("/register/paciente", function(req, res) {
+router.post("/register", function(req, res) {
     var name = req.body.username;
     var pass = req.body.password;
-    Paciente.register(new Paciente({username: name}), pass, function(err, paciente){
-        if(err){
-            req.flash("error", err.message);
-            return res.redirect("register/paciente");
-        } 
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Bem vindo à Clínica " + paciente.username);
-            res.redirect("/");     
-        });
-    });
+    // Paciente.register(new Paciente({username: name}), pass, function(err, paciente){
+    //     if(err){
+    //         req.flash("error", err.message);
+    //         return res.redirect("register/paciente");
+    //     } 
+    //     passport.authenticate("local")(req, res, function(){
+    //         req.flash("success", "Bem vindo à Clínica " + paciente.username);
+    //         res.redirect("/");     
+    //     });
+    // });
+    console.log(name);
+    console.log(pass);
+    console.log(req.body.email);
 })
 
 //show login form
